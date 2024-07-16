5413cbd20183109ce6d044fa1e41a35c66ab27f3
Ruan pablo <ruansaraiva555@gmail.com>
2024-07-15 21:43:14 -0300

    Project created

---
+++ src/db/dbException.java
@@ -0,0 +1,16 @@
+package db;
+
+public class dbException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public dbException(String msg) {
+		super(msg);
+		
+	}
+
+}
