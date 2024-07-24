3199353eb9ff9f7f40cc0cf832f7bad43dab9d06
Ruan pablo <ruansaraiva555@gmail.com>
2024-07-23 21:32:11 -0300

    Seller entity class

--- src/apliccation/Program.java
+++ src/apliccation/Program.java
@@ -1,6 +1,9 @@
 package apliccation;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,10 @@ public class Program {
 		Department obj = new Department(1,"Books");
 		
 		System.out.println(obj);
+		
+		Seller seller = new Seller(1,"bob","bob@gmail.com",new Date(),3000.0,obj);
+		
+		System.out.println(seller);
 		
 		
 
