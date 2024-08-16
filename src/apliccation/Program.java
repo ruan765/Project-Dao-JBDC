bad34ec0a6be61b1fabee954b38bfc9c2a02c1b6
Ruan pablo <ruansaraiva555@gmail.com>
2024-08-15 21:40:56 -0300

    FindById Implementation

--- src/apliccation/Program.java
+++ src/apliccation/Program.java
@@ -10,16 +10,14 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1,"Books");
-		
-		System.out.println(obj);
-		
-		Seller seller = new Seller(1,"bob","bob@gmail.com",new Date(),3000.0,obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
+		
+	
 		
 		
 
